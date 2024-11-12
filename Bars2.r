65d68b04a8d686fafb1499e18adc7601530778b8
Twelve30 <51714848+Twelve30@users.noreply.github.com>
2024-11-12 22:22:24 +0530

    Add files via upload

---
+++ Bars2.r
@@ -0,0 +1,5 @@
+p = ggplot(datad, aes(x=Protein, y=number, fill="blue")) + geom_bar(stat="identity")
+p = p + theme_bw() + geom_text(aes(label=number), vjust=-0.3, size=2.5, hjust=0.5) + theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+p = p + xlab("Proteins") + ylab("Number of Organisms") + ggtitle("Distribution of Moonlight Proteins in Bacteria") + theme(axis.line = element_line(colour = "black"))
+p
+ggsave("Project graph.pdf", width=4096, height = 2160, units = "px")
