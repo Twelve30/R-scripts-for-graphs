e42b3fdd99919b695a8524b094a7324f5af6d22b
Twelve30 <51714848+Twelve30@users.noreply.github.com>
2025-04-11 16:23:54 +0530

    Barplot with error bars and individual data points

---
+++ PLA2.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+library(ggpmisc)
+library(poorman)
+library(matrixStats)
+library(ggthemes)
+datad = read.csv("PLA2.csv")
+newdata2 = datad %>% select(Sample, Well1, Well2, Well3) %>% mutate(Absorb = rowMeans(datad[c("Well1", "Well2", "Well3")])) %>% mutate(deviation = rowSds(as.matrix(datad[c("Well1", "Well2", "Well3")])))
+newdata2 = newdata2 %>% mutate(error = deviation/sqrt(3))
+p = ggplot(newdata2, aes(x = Sample)) + geom_errorbar(aes(y = Absorb, ymin = Absorb - error, ymax = Absorb + error), width = 0.15) + geom_col(aes(y = Absorb, color=Sample), width = 0.5, fill = NA)
+p = p + geom_point(aes(y = Well1), position = position_nudge(x = 0.1), shape = 17, size = 0.5) +
+  geom_point(aes(y = Well2), position = position_nudge(x = -0.1), shape = 17, size = 0.5) +
+  geom_point(aes(y = Well3), position = position_nudge(x = -0.1), shape = 17, size = 0.5)
+p = p + xlab("Extract") + ylab("Absorbance") + ggtitle("PLA2 Assay (NOB Method)")
+p = p + theme_few() + theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 1.1, hjust=1))
+print(p)
+ggsave("PLA2 test.svg", width = 9.31, height = 10.27, units = "cm")
