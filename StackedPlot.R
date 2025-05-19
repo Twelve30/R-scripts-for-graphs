ab43b16be430de881c4fba4b8b931dc9ff009045
Twelve30 <51714848+Twelve30@users.noreply.github.com>
2025-05-19 22:28:01 +0530

    Stacked scatter plots

--- StackedPlot.R
+++ StackedPlot.R
@@ -2,7 +2,7 @@ library(ggplot2)
 
 datad = read.csv("Past.csv")
 
-p = ggplot(datad, aes(x = Yr, y = Value)) + geom_point(aes(color=Type)) + 
+p = ggplot(datad, aes(x = Year, y = Value)) + geom_point(aes(color=Type)) + 
   geom_line(aes(color=Type)) + xlab("Year") + labs(title = "Changes in Global Carbon dioxide Concentration and Temperature in past 65  years ") +
   labs(y = "") +
   scale_y_continuous(expand = c(0.1, 0.1)) +
