c01a2ec0d490b6b825e4ff292e1f1ccc452f5c8f
Twelve30 <51714848+Twelve30@users.noreply.github.com>
2025-05-19 21:48:58 +0530

    Update Ordered bar.R

--- Ordered bar.R
+++ Ordered bar.R
@@ -1,17 +1,24 @@
 library(ggplot2)
 library(ggpmisc)
-library(poorman)
+library(dplyr)
 library(matrixStats)
 library(ggthemes)
 datad = read.csv("WST.csv")
-newdata2 = datad %>% select(Sample, Well1, Well2, Well3, order) %>% mutate(Absorb = rowMeans(datad[c("Well1", "Well2", "Well3")])) %>% mutate(deviation = rowSds(as.matrix(datad[c("Well1", "Well2", "Well3")])))
-newdata2 = newdata2 %>% mutate(error = deviation/sqrt(3)) %>% mutate(tAbsorb = round(Absorb, digits = 1))
+newdata2 = datad %>% 
+  select(Sample, Well1, Well2, Well3, order) %>%
+  mutate(Absorb = rowMeans(datad[c("Well1", "Well2", "Well3")])) %>%
+  mutate(deviation = rowSds(as.matrix(datad[c("Well1", "Well2", "Well3")]))) %>%
+  mutate(error = deviation/sqrt(3)) %>%
+  mutate(tAbsorb = round(Absorb, digits = 1))
+
 p = ggplot(newdata2, aes(x = reorder(Sample, order))) + geom_errorbar(aes(y = Absorb, ymin = Absorb - error, ymax = Absorb + error), width = 0.15) + geom_col(aes(y = Absorb, color=Sample), width = 0.5, fill = NA)
 p = p + geom_point(aes(y = Well1), position = position_nudge(x = 0.1), shape = 17, size = 0.5) +
   geom_point(aes(y = Well2), position = position_nudge(x = -0.1), shape = 17, size = 0.5) +
   geom_point(aes(y = Well3), position = position_nudge(x = -0.1), shape = 17, size = 0.5)
+
 p = p + xlab("Sample") + ylab("Percent viability") + ggtitle("Cytotoxicity Assay")
 p = p + geom_text(aes(y = Absorb + error, label = tAbsorb), size = 2.5, vjust = -0.75)
 p = p + theme_few() + theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 1.1, hjust=1))
+
 print(p)
 ggsave("WST.svg", width = 18, height = 15, units = "cm")
