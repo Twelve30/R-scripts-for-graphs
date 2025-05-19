4c77bcea2a30e3a2e8eba6e942c548d7a67755f1
Twelve30 <51714848+Twelve30@users.noreply.github.com>
2025-05-19 22:19:24 +0530

    Delete Piechart.R

--- Piechart.R
+++
@@ -1,5 +0,0 @@
-library(ggplot2)
-library(ggpubr)
-datad = read.csv("Book1.csv")
-pie = ggpie(datad, "Number", fill = "Species", title = "Frequency of Moonlighting proteins") + theme(legend.position = "right")
-pie
