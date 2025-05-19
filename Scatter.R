05c824883311f4152c85653f8eb4191ff46f8a66
Twelve30 <51714848+Twelve30@users.noreply.github.com>
2025-05-19 22:23:58 +0530

    Simple scatterplot with linear trendline, equation and r2

--- Scatter.R
+++ Scatter.R
@@ -1,9 +1,12 @@
 library(ggplot2)
 library(ggpmisc)
 library(ggpp)
+
 pdata = read.csv("Data.csv")
+
 p = ggplot(pdata, aes(x=Concentration, y=Absorbance)) + geom_point() + geom_smooth(method = "lm", se=FALSE)
 p = p + ggtitle("Standard Curve of Paracetamol") + xlab("Concentration (mg/ml)") + ylab("Absorbance")
 p = p + stat_poly_eq(use_label(c("eq", "R2")))
-p
+
+print(p)
 ggsave("PCM.pdf", width = 3840, height = 2160, units = "px")
