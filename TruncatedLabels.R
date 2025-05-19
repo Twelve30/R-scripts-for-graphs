708ecf1fb84b8a6f972e878df70a35397fab382d
Twelve30 <51714848+Twelve30@users.noreply.github.com>
2025-05-19 21:46:11 +0530

    Update TruncatedLabels.R

--- TruncatedLabels.R
+++ TruncatedLabels.R
@@ -1,17 +1,25 @@
 library(ggplot2)
-library(ggpmisc)
-library(poorman)
+library(dplyr)
 library(matrixStats)
 library(ggthemes)
+
 datad = read.csv("PLA2.csv")
-newdata2 = datad %>% select(Sample, Well1, Well2, Well3) %>% mutate(Absorb = rowMeans(datad[c("Well1", "Well2", "Well3")])) %>% mutate(deviation = rowSds(as.matrix(datad[c("Well1", "Well2", "Well3")])))
-newdata2 = newdata2 %>% mutate(error = deviation/sqrt(3)) %>% mutate(tAbsorb = round(Absorb, digits = 1))
+
+newdata2 = datad %>%
+  select(Sample, Well1, Well2, Well3) %>%
+  mutate(Absorb = rowMeans(datad[c("Well1", "Well2", "Well3")])) %>%
+  mutate(deviation = rowSds(as.matrix(datad[c("Well1", "Well2", "Well3")]))) %>%
+  mutate(error = deviation/sqrt(3)) %>%
+  mutate(tAbsorb = round(Absorb, digits = 1))
+
 p = ggplot(newdata2, aes(x = Sample)) + geom_errorbar(aes(y = Absorb, ymin = Absorb - error, ymax = Absorb + error), width = 0.15) + geom_col(aes(y = Absorb, color=Sample), width = 0.5, fill = NA)
 p = p + geom_point(aes(y = Well1), position = position_nudge(x = 0.1), shape = 17, size = 0.5) +
   geom_point(aes(y = Well2), position = position_nudge(x = -0.1), shape = 17, size = 0.5) +
   geom_point(aes(y = Well3), position = position_nudge(x = -0.1), shape = 17, size = 0.5)
 p = p + geom_text(aes(y = Absorb + error, label = tAbsorb), size = 2.5, vjust = -0.75)
+
 p = p + xlab("Venom:Plant Extract 1:100") + ylab("Percent Inhibition") + ggtitle("PLA2 Assay")
 p = p + theme_few() + theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 1.1, hjust=1))
+
 print(p)
-ggsave("Protease.svg", width = 16.31, height = 15.27, units = "cm")
+ggsave("Significant.svg", width = 16.31, height = 15.27, units = "cm")
