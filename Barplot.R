306fceac2f121b661592470b98e310056da49271
Twelve30 <51714848+Twelve30@users.noreply.github.com>
2025-05-19 22:13:17 +0530

    Simple barplot

--- Barplot.R
+++ Barplot.R
@@ -1,5 +1,7 @@
 library(ggplot2)
+
 p = ggplot(maindata, aes(x=Primary.function, y=number.of.organisms, fill=Primary.function)) + geom_bar(stat = "identity")
 p = p + theme_minimal() + geom_text(aes(label=number.of.organisms), hjust=-0.3, size=3.5) + theme(legend.position = "none") + coord_flip() 
-p
+print(p)
+
 ggsave("barplot.pdf", width = 3840, height = 2160, units = "px")
