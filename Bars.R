348633c793f406597aa089126edf5cbcf6182b32
Twelve30 <51714848+Twelve30@users.noreply.github.com>
2025-05-19 22:15:52 +0530

    Advanced barplot

--- Bars.R
+++ Bars.R
@@ -4,6 +4,6 @@ p = ggplot(datad, aes(fill=Type, y=Index, x=Time)) + geom_bar(position='dodge', 
 p = p + ggtitle("Plot of Analgesic Index vs Time") + xlab("Time (in minutes)") + ylab("Analgesic Index")
 p = p + labs(fill = "Drug used")
 p = p + geom_text(aes(label=Index), vjust=-0.3, size=3.5, position = position_dodge(8))
-print(p)
 
+print(p)
 ggsave("Plot.pdf", width = 72.5, height = 33.5, units = "cm" )
