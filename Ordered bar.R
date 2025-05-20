e4ee626b87697cdab20c8f0bfd253fb2882c3806
Twelve30 <51714848+Twelve30@users.noreply.github.com>
2025-05-20 22:18:10 +0530

    Update Ordered bar.R

--- Ordered bar.R
+++ Ordered bar.R
@@ -13,14 +13,22 @@ newdata2 = datad %>%
   mutate(error = deviation/sqrt(3)) %>%
   mutate(tAbsorb = round(Absorb, digits = 1))
 
-p = ggplot(newdata2, aes(x = reorder(Sample, order))) + geom_errorbar(aes(y = Absorb, ymin = Absorb - error, ymax = Absorb + error), width = 0.15) + geom_col(aes(y = Absorb, color=Sample), width = 0.5, fill = NA)
+p = ggplot(newdata2, aes(x = reorder(Sample, order))) +
+  geom_errorbar(aes(y = Absorb, ymin = Absorb - error, ymax = Absorb + error), width = 0.15) +
+  geom_col(aes(y = Absorb, color=Sample), width = 0.5, fill = NA)
+
 p = p + geom_point(aes(y = Well1), position = position_nudge(x = 0.1), shape = 17, size = 0.5) +
   geom_point(aes(y = Well2), position = position_nudge(x = -0.1), shape = 17, size = 0.5) +
   geom_point(aes(y = Well3), position = position_nudge(x = -0.1), shape = 17, size = 0.5)
 
-p = p + xlab("Sample") + ylab("Percent viability") + ggtitle("Cytotoxicity Assay")
 p = p + geom_text(aes(y = Absorb + error, label = tAbsorb), size = 2.5, vjust = -0.75)
-p = p + theme_few() + theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 1.1, hjust=1))
+
+p = p + ggtitle("Cytotoxicity Assay") +
+  xlab("Sample") +
+  ylab("Percent viability")
+
+p = p + theme_few() +
+  theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 1.1, hjust=1))
 
 print(p)
 ggsave("WST.svg", width = 18, height = 15, units = "cm")
