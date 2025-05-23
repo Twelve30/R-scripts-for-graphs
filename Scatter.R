7edd577760c621dbd2506dd8fc8c00d0a5962b51
Twelve30 <51714848+Twelve30@users.noreply.github.com>
2025-05-23 13:56:23 +0530

    Update Scatter.R

--- Scatter.R
+++ Scatter.R
@@ -4,9 +4,13 @@ library(ggpp)
 
 pdata = read.csv("Data.csv")
 
-p = ggplot(pdata, aes(x=Concentration, y=Absorbance)) + geom_point() + geom_smooth(method = "lm", se=FALSE)
+p = ggplot(pdata, aes(x=Concentration, y=Absorbance)) +
+  geom_point() +
+  geom_smooth(method = "lm", se=FALSE) +
+  stat_poly_eq(use_label(c("eq", "R2")))
+
 p = p + ggtitle("Standard Curve of Paracetamol") + xlab("Concentration (mg/ml)") + ylab("Absorbance")
-p = p + stat_poly_eq(use_label(c("eq", "R2")))
+
 
 print(p)
 ggsave("PCM.pdf", width = 3840, height = 2160, units = "px")
